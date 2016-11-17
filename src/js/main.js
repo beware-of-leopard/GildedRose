427faee0aac34552dea58f15bac5b37702ee87ee
DANIEL REVO <DANIELREVO08@GMAIL.COM>
2016-11-16 23:13:39 -0500

    all tests now pass in "in-place" branch

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,17 @@
-function fizz () {
+function fizz (a) {
   // TODO
+  return a;
 };
 
 function fizzBuzz (start, end) {
   // Write fizzBuzz here once fizz is finished.
+  let fizzBuzzArray=[];
+  let i;
+  for (i=start; i<=end; i++){
+  	fizzBuzzArray.push(i);
+  }
+
+  return fizzBuzzArray;
 };
 
-export { fizz };
+export { fizz, fizzBuzz};
